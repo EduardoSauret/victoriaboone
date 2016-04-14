fd24efcf46d88a69dc6c59f69fbe90ac60db94ff
eduardosauret <eduardo.sauret@gmail.com>
2016-04-14 11:24:46 -0500

    fullpage crashed

--- client/app/app.js
+++ client/app/app.js
@@ -1,19 +1,18 @@
 'use strict';
 
 angular.module('victoriabooneApp', [
-  'victoriabooneApp.constants',
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ui.router',
-  'ui.bootstrap',
-  'ngAnimate',
-  'fullPage.js'
-])
-  .config(function($urlRouterProvider, $locationProvider) {
+        'victoriabooneApp.constants',
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ui.router',
+        'ui.bootstrap',
+        'ngAnimate'
+    ])
+    .config(function($urlRouterProvider, $locationProvider) {
 
-    $urlRouterProvider
-      .otherwise('/');
+        $urlRouterProvider
+            .otherwise('/');
 
-    $locationProvider.html5Mode(true);
-  });
+        $locationProvider.html5Mode(true);
+    });
