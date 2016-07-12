62309eae2a7562153e4e43c5999fde2e0999606e
eduardosauret <eduardo.sauret@gmail.com>
2016-07-11 19:43:28 -0500

    images resized spring01

--- client/components/fullpage/fullpage.controller.js
+++ client/components/fullpage/fullpage.controller.js
@@ -20,7 +20,7 @@
         this.slides = [{
             title: 'Simple',
             description: 'Easy to use. Configurable and customizable.',
-            //src: 'assets/images/IMG_8743.jpg'
+            //src: 'assets/images/spring01/IMG_01.jpg'
         }, {
             title: 'Cool',
             description: 'It just looks cool. Impress everybody with a simple and modern web design!',
